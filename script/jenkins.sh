516ec4ccd946bd127df250119842b1a37462eb79
Alex Dean <github@mostlyalex.com>
2016-03-29 15:05:02 -0500

    updates for jenkins
    add gems into Gemfile.lock. remove db stuff from jenkins.sh since the gem
    doesn't need a db for testing

--- script/jenkins.sh
+++ script/jenkins.sh
@@ -19,51 +19,6 @@ if [ $? -eq 1 ]; then
   gem install bundler
 fi
 
-export DB_DATABASE=''
-
-# you can delete this block once you've set DB_DATABASE and dealt with
-# database-local.yml support.
-if [ -z "${DB_DATABASE}" ]; then
-  echo "you need to do a little db setup..."
-  echo
-  echo "#1: edit jenkins.sh, set a value for DB_DATABASE, and commit/push the result"
-  echo "#2: update your application.rb to support config/database-local.yml"
-  echo "    example:"
-  echo
-  echo "      module YourApp"
-  echo "        class Application < Rails::Application"
-  echo "          # load database-local.yml instead of database.yml if it exists"
-  echo "          local_db_file = 'config/database-local.yml'"
-  echo "          paths['config/database'] = [local_db_file] if File.exist?(local_db_file)"
-  echo "        end"
-  echo "      end"
-  echo
-  echo "    you don't absolutely have to do this"
-  echo "    but the default script/jenkins.sh expects it."
-  exit 1
-fi
-
-# Exit immediately if any single command fails
-set -e
-
-cp $DATABASE_YAML $WORKSPACE/config/database-local.yml
-
-# Setup the database config using the password provided by Jenkins
-if [ -z "${DB_PASSWORD}" ]; then
-  echo "DB_PASSWORD is not set, check credential-store plugin and job config"
-  exit 1
-fi
-
-if [ -z "${DATABASE_YAML}" ]; then
-  echo "DATABASE_YAML is not set, check jenkins job config"
-  exit 1
-fi
-
-# use jenkins-specific secrets file if it exists
-if [ -e $WORKSPACE/config/secrets.jenkins.yml ]; then
-  cp $WORKSPACE/config/secrets.jenkins.yml $WORKSPACE/config/secrets.yml
-fi
-
 # Ensure we have ruby
 ruby --version
 echo "Ruby Gemset:"
