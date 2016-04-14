c3230dfe57312d3fc506e36d24cad9c0970b31e5
Clay <clay@ted.com>
2016-04-14 16:16:22 -0500

    Added basic json logger tests & made pass

--- spec/json_logger_spec.rb
+++ spec/json_logger_spec.rb
@@ -1,7 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "JsonLogger" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe 'JsonLogger' do
+  it 'Returns a new JsonLogger with default options' do
+    expect(JsonLogger.factory).to_not eq nil
+  end
+
+  it 'Sets device, level & progname options' do
+    options = {
+      level: Logger::INFO,
+      progname: 'myfile',
+      device: STDOUT
+    }
+    logger = JsonLogger.factory(options)
+    expect(logger.level).to eq 1
+    expect(logger.progname).to eq 'myfile'
   end
 end
